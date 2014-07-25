41bb078dbdab77f171cd9ada58a9b1a9563fd1af
felixbarnsteiner <f.barnsteiner@isys-software.de>
2014-07-25 17:02:37 +0200

    changed intro image

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -6,38 +6,6 @@
 
 (function ($) {
 
-	/*---------------------------------------------------- */
-	/* Mobile Menu
-	 ------------------------------------------------------ */
-	var toggle_button = $("<a>", {
-			id: "toggle-btn",
-			html: "Menu",
-			title: "Menu",
-			href: "#" }
-	);
-	var nav_wrap = $('nav#nav-wrap');
-	var nav = $("ul#nav");
-
-	/* id JS is enabled, remove the two a.mobile-btns
-	 and dynamically prepend a.toggle-btn to #nav-wrap */
-	nav_wrap.find('a.mobile-btn').remove();
-	nav_wrap.prepend(toggle_button);
-
-	toggle_button.on("click", function (e) {
-		e.preventDefault();
-		nav.slideToggle("fast");
-	});
-
-	if (toggle_button.is(':visible')) nav.addClass('mobile');
-	$(window).resize(function () {
-		if (toggle_button.is(':visible')) nav.addClass('mobile');
-		else nav.removeClass('mobile');
-	});
-
-	$('ul#nav li a').on("click", function () {
-		if (nav.hasClass('mobile')) nav.fadeOut('fast');
-	});
-
 	/*----------------------------------------------------*/
 	/* Smooth Scrolling
 	 ------------------------------------------------------ */
