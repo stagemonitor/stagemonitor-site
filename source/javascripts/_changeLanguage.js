c83179a73faf0a46769b8cf44c361c8958e98932
Fabian Trampusch <fabian.trampusch@freenet.de>
2017-10-27 14:19:54 +0200

    change indentation to tabs

--- source/javascripts/_changeLanguage.js
+++ source/javascripts/_changeLanguage.js
@@ -2,33 +2,33 @@ $( ".language" ).click(function() {
 	localStorage.setItem('language', $(this).attr("id"));
 	if($(this).attr("id") === "en"){
 		window.location.href = "/";
-	}else {
+	} else {
 		window.location.href = $(this).attr("id");
 	}
 });
 
 
 $(function() {
-    var language = localStorage.getItem('language');
-    if(!language){
-        language = getBrowserLanguage();
-    }
-    if(language.length > 0 && language !== "en" && window.location.pathname === "/"){
-        $( "#"+language).click();
-    }
+	var language = localStorage.getItem('language');
+	if (!language){
+		language = getBrowserLanguage();
+	}
+	if (language.length > 0 && language !== "en" && window.location.pathname === "/"){
+		$( "#"+language).click();
+	}
 });
 
 function getBrowserLanguage(){
 	var language;
-    if (navigator.languages != undefined) {
-        language = navigator.languages[0];
-    } else {
-        language = navigator.language;
-    }
+	if (navigator.languages != undefined) {
+		language = navigator.languages[0];
+	} else {
+		language = navigator.language;
+	}
 
-    if(language.indexOf("-") > 0){
+	if (language.indexOf("-") > 0){
 		language = language.substring(0,language.indexOf("-"));
 	}
-	
-    return language;
+
+	return language;
 }
